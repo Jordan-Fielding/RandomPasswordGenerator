4739c9bcbab18f17eeeae6db3c393ed3774013d3
Jordan-Fielding <107819004+Jordan-Fielding@users.noreply.github.com>
2022-06-21 09:26:01 +1000

    PAssword Generator

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,14 @@
+import javafx.animation.RotateTransition;
 import javafx.application.Application;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.io.FileReader;
-import java.util.Random;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.util.Duration;
+
+import java.io.IOException;
 
 
 public class Main extends Application {
@@ -25,9 +23,9 @@ public class Main extends Application {
         //First Scene - Label Button1 and Button Action(Generate Random Password)
         Label labelalert = new Label("Generate Random Password");
         labelalert.setStyle("-fx-text-fill: #E8E8E8");
+
+        //Scene1 Button
         Button buttonalert = new Button("Generate");
-
-
         buttonalert.setOnAction(e -> {
             try {
                 AlertBox.display();
@@ -35,6 +33,24 @@ public class Main extends Application {
                 throw new RuntimeException(ex);
             }
         });
+
+        //Creating a rotate transition for Button
+        RotateTransition rotateTransition = new RotateTransition();
+        //Setting the duration for the transition
+        rotateTransition.setDuration(Duration.millis(200));
+        //Setting the node for the transition
+        rotateTransition.setNode(buttonalert);
+        //Setting the angle of the rotation
+        rotateTransition.setByAngle(15);
+        //Setting the cycle count for the transition
+        rotateTransition.setCycleCount(4000);
+        //Setting auto reverse value to false
+        rotateTransition.setAutoReverse(true);
+        //Playing the animation
+        rotateTransition.play();
+
+
+
 
         //Scene 1 VBox
         VBox layout1 = new VBox(20);
